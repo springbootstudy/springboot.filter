fa5a0c8f4daafae1eed699e8ffafea16264b4e22
libin <libin2@ctsi.com.cn>
2018-08-10 10:44:45 +0800

    通过注解配置bean注入

--- src/main/java/com/ctsi/springboot/token/Application.java
+++ src/main/java/com/ctsi/springboot/token/Application.java
@@ -3,7 +3,10 @@ package com.ctsi.springboot.token;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.ctsi.springboot.token.mydefine.EnableMyService;
+
 @SpringBootApplication
+@EnableMyService
 public class Application {
 
 	public static void main(String[] args) {
